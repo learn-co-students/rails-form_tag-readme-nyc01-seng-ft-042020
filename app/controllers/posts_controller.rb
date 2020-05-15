f171b9f27b16295cb1db75e120a0a31c13455724
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-05-14 22:00:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+    @post=Post.new
+  end
+
+  def create
+    @post=Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+ #
+
+#   def post_params
+#     params.require(:posts).permit(:title,:description)
+#  end
+
 end
