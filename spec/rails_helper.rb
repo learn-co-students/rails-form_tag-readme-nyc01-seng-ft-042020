3d4afeeef6ce4958cb0113f1686e8fc63e9838f2
carolineglass <carolineglass89@gmail.com>
2020-05-15 17:17:54 -0400

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,15 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'rails_helper'
+ 
+describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
